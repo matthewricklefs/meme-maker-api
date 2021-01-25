912e1ee7e496daa39c026fe74861919cdca4ed45
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-25 12:59:40 -0500

    adds connection string and the actual environment .env

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_heroku import Heroku
+from settings import DATABASE_URL
 import os
 
 app = Flask(__name__)
 heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 CORS(app)
 
